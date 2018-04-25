1deb2567b19ee87a4aa810a42d9d2bd61c339f75
Efosa Oyegun <efosao@gmail.com>
2018-04-25 01:00:59 -0700

    added code to calculate image dimensions, and to merge the uploaded images using the `merge-images` plugin

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Dropzone from 'react-dropzone'
+import mergeImages from 'merge-images'
 
 class Uploader extends Component {
   state = {
@@ -7,57 +8,108 @@ class Uploader extends Component {
     currFileIdx: 0,
     maxFiles: 4
   }
-  onDrop = (files) => {
+
+  componentDidUpdate () {
+    this.drawImages()
+  }
+
+  onImageDrop = (files) => {
     const { currFileIdx, images } = this.state
-    console.log('you dropped stuff on me', files)
-    const { updateImage } = this
+    const { updateImagesInState } = this
     for (let idx in files) {
       const file = files[idx]
-      console.log('file', file)
       const reader  = new FileReader();
       reader.onload = function(e)  {
-          const image = document.createElement("img");
-          const data = e.target.result
-          console.log('data', data)
-          image.src = data
-          document.body.appendChild(image)
-          images[currFileIdx] = {
+        const data = e.target.result
+        const img  = document.createElement('img')
+        img.src = data
+        img.addEventListener('load', () => {
+          // get image width
+          // image is loaded into DOM so the `width` can be read
+          // image is immediately removed afterwards
+          document.body.removeChild(img)
+          const { width } = img
+
+          // clone image collection
+          const newImgArray = images.map(o => ({ ...o }))
+          newImgArray[currFileIdx] = {
             data,
-            name: file.name
+            name: file.name,
+            width
           }
-          updateImage(images)
-        }
+
+          updateImagesInState(newImgArray)
+        })
+        document.body.appendChild(img)
+      }
+
       reader.readAsDataURL(file);
-      
     }
   }
 
-  updateImage = (images) => {
+  updateImagesInState = (images) => {
     const { currFileIdx, maxFiles } = this.state
     const nextFileIdx = (currFileIdx < (maxFiles - 1)) ? (currFileIdx + 1) : 0
-    console.log('nextFileIdx', nextFileIdx)
     this.setState({
       currFileIdx: nextFileIdx,
       images
     })
   }
 
+  getImageMergeOptions = () => {
+    const { images } = this.state
+    // we are calculating the maximum width of the [canvas] needed to hold the image
+    const width = images.reduce((acc, curr, idx) => (curr.width + acc), 0)
+    return {
+      width
+    }
+  }
+
+  drawImages = () => {
+    const { currFileIdx, images } = this.state
+    let cummHorizontalWidth = 0
+
+    // format image data for `merge-images` plugin
+    const imgDataArr = images.map(({ data, width }, idx) => {
+      const result = {
+        src: data,
+        x: cummHorizontalWidth,
+        y: 0
+      }
+      cummHorizontalWidth += width
+      return result
+    })
+
+    const options = this.getImageMergeOptions()
+
+    // draw the images into empty image tag in view
+    mergeImages(imgDataArr, options)
+      .then(b64 => document.querySelector('img').src = b64)
+  }
+
   render () {
-    console.log('this.state', this.state)
     return (
       <div>
         <link href="https://fonts.googleapis.com/css?family=Tajawal" rel="stylesheet" />
         <h1>Image Stitcher</h1>
         <h2>By Efosa Oyegun</h2>
         
-          <Dropzone onDrop={this.onDrop}>
+          <Dropzone onDrop={this.onImageDrop}>
             <div className='drop'>
-                <p>Drop your images in the box below</p>
+                <p>Click or Drop your images in this box</p>
             </div>
           </Dropzone>
-        <style jsx>{`
+
+          <img />
+
+        <style global jsx>{`
+          body {
+            font-family: "Tajawal", sans-serif;
+          }
           .drop {
             background-color: lightgreen;
+            font-size: 24px;
+            font-weight: bold;
             height: 100%;
             width: 100%;
           }
@@ -65,16 +117,10 @@ class Uploader extends Component {
             margin: 0;
             padding: 10px;
           }
-        `}</style>
-        
-        <style global jsx>{`
-          body {
-            font-family: "Tajawal", sans-serif;
-          }
-          drop {
-            color: red;
-            height: 100px;
-            width: 100px;
+          img {
+            border: 4px solid pink;
+            margin-top: 20px;
+            max-width: 80%;
           }
         `}</style>
       </div>
