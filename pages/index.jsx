5892e413abbda59a4f32a0d40dbf1c8b8cf0fa1a
Efosa Oyegun <efosao@gmail.com>
2018-04-24 22:24:42 -0700

    added react-dropzone to web page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,37 @@
-export default () => (
-  <div>
-    <link href="https://fonts.googleapis.com/css?family=Tajawal" rel="stylesheet" />
-    <h1>Welcome to next.js!</h1>
-    <p>Welcome to next.js!</p>
-    <p>Welcome to next.js!</p>
-    <p>Welcome to next.js!</p>
-    <p>Welcome to next.js!</p>
-    <p>Welcome to next.js!</p>
-    <style global jsx>{`
-      body {
-        font-family: "Tajawal", sans-serif;
-      }
-    `}</style>
-  </div>
-)
+import React, { Component } from 'react'
+import Dropzone from 'react-dropzone'
 
+class Uploader extends Component {
+  onDrop = (files) => {
+    console.log('you dropped stuff on me', files)
+  }
+  render () {
+    return (
+      <div>
+        <link href="https://fonts.googleapis.com/css?family=Tajawal" rel="stylesheet" />
+        <h1>Welcome to next.js!</h1>
+        <Dropzone onDrop={this.onDrop}><div className='drop'>ddd</div></Dropzone>
+        <style jsx>{`
+          .drop {
+            background-color: red;
+            height: 100px;
+            width: 100px;
+          }
+        `}</style>
+        <p>Welcome to next.js!</p>
+        <style global jsx>{`
+          body {
+            font-family: "Tajawal", sans-serif;
+          }
+          drop {
+            color: red;
+            height: 100px;
+            width: 100px;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
+export default Uploader
