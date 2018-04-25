2b3cb96a7ea10c0cfa1386e80d06c76057c146cd
Efosa Oyegun <efosao@gmail.com>
2018-04-24 23:01:48 -0700

    added logic for storing images with state and looping through an array to store them

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,29 @@ import React, { Component } from 'react'
 import Dropzone from 'react-dropzone'
 
 class Uploader extends Component {
+  state = {
+    images: [],
+    currFileIdx: 0,
+    maxFiles: 4
+  }
   onDrop = (files) => {
+    const { images, currFileIdx, maxFiles } = this.state
     console.log('you dropped stuff on me', files)
+    for (let idx in files) {
+      const file = files[idx]
+      console.log('file', file)
+      const nextFileIdx = (currFileIdx < (maxFiles - 1))
+        ? (currFileIdx + 1) : 0
+      console.log('nextFileIdx', nextFileIdx)
+      images[currFileIdx] = { name: file.name }
+      this.setState({
+        currFileIdx: nextFileIdx,
+        images
+      })
+    }
   }
   render () {
+    console.log('this.state', this.state)
     return (
       <div>
         <link href="https://fonts.googleapis.com/css?family=Tajawal" rel="stylesheet" />
